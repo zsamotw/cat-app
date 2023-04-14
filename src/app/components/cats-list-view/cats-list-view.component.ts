2b8e86a252d40cfa0aca8a77f91f2a8db1f2b708
zsamotw <donkemot@interia.pl>
2023-04-14 19:52:19 +0200

    Update card styles and loading.

--- src/app/components/cats-list-view/cats-list-view.component.ts
+++ src/app/components/cats-list-view/cats-list-view.component.ts
@@ -5,29 +5,13 @@ import { Component, ElementRef, EventEmitter, Input, OnChanges, Output, SimpleCh
   templateUrl: './cats-list-view.component.html',
   styleUrls: ['./cats-list-view.component.scss']
 })
-export class CatsListViewComponent implements OnChanges {
-  @ViewChild('scrollContainer') scrollContainer?: ElementRef<HTMLDivElement>;
-
+export class CatsListViewComponent {
   @Input() items: string[] = [];
   @Input() isError = false;
   @Input() isLoading = false;
   @Input() numberOfSkeletons = 0;
 
   @Output() loadMoreItems = new EventEmitter();
-
-  showLoadButton = false;
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (!changes['isLoading']?.currentValue) {
-      this.showLoadButton = !this.hasScroll();
-    } else {
-      this.showLoadButton = false;
-    }
-  }
-
-  private hasScroll() {
-    return this.scrollContainer ? this.scrollContainer.nativeElement.clientHeight < this.scrollContainer.nativeElement.scrollHeight : false;
-  }
 
   loadMore() {
     this.loadMoreItems.emit();
